de787b2d8bfff8b7a67871fb81faf409dd21ae77
sevenseablue <sevenseablue@gmail.com>
2019-09-17 15:23:11 +0800

    hive command test

---
+++ ql/src/test/org/apache/hadoop/hive/ql/processors/HiveCommandTest.java
@@ -0,0 +1,22 @@
+package org.apache.hadoop.hive.ql.processors;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HiveCommandTest {
+    HiveCommand hiveCommand = HiveCommand.DFS;
+    @Test
+    public void isOnlyForTesting() {
+
+    }
+
+    @Test
+    public void find() {
+        System.out.println(hiveCommand);
+    }
+
+    @Test
+    public void find1() {
+    }
+}
